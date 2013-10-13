ad61b6285968e47d115de5acb84d2f38fcecd4ed
Andrey Dankevich <dankevicha@gmail.com>
2013-10-13 18:11:19 +0300

    Implemented data mining

--- TestRegex/TestRegex/TestRegex.cpp
+++ TestRegex/TestRegex/TestRegex.cpp
@@ -8,19 +8,97 @@
 #include <regex>
 #include <vector>
 #include <array>
+#include <unordered_set>
 
-enum Replacements {
+struct Rule;
+
+typedef std::vector<Rule> RULES_CONT;
+typedef std::unordered_set<std::string> RESULTS_CONT;
+typedef std::unordered_set<std::string> TYPES_CONT;
+
+RULES_CONT rules;
+RESULTS_CONT results;
+TYPES_CONT types;
+
+enum Replacement {
+	INVALID = -1,
 	ID = 0,
 	NAME,
 	RESULT,
 	TYPE,
 };
 
+bool is_valid_id ( std::string const& value )
+{
+	std::regex const rx ( "\\d+|\\d+.*\\(\\d+\\)" );
+
+	return std::regex_match ( value, rx );
+}
+
+bool is_valid_result ( std::string const& value )
+{
+	return ( results.find ( value ) != results.end () );
+}
+
+bool is_valid_type ( std::string const& value )
+{
+	return ( types.find ( value ) != types.end () );
+}
+
+bool is_value_correct ( std::string const& value, Replacement const replacement )
+{
+	bool result = true;
+
+	switch ( replacement )
+	{
+	case ID:
+		result = is_valid_id ( value );
+		break;
+
+	case RESULT:
+		result = is_valid_result ( value );
+		break;
+
+	case TYPE:
+		result = is_valid_type ( value );
+		break;
+	}
+
+	return result;
+}
+
+Replacement string2replacement ( std::string const& str )
+{
+	auto result = INVALID;
+
+	if ( str == "$ID$" )
+		result = ID;
+	else if ( str == "$NAME$" )
+		result = NAME;
+	else if ( str == "$RESULT$" )
+		result = RESULT;	
+	else if ( str == "$TYPE$" )
+		result = TYPE;
+
+	return result;
+}
+
+
+struct ErrorData
+{
+	std::string original;
+	std::string id;
+	std::string type;
+	std::string result;
+	std::string rule_str;
+	bool valid;
+};
+
 struct Rule
 {
 	std::string raw_str;	// "Unmatched - $ID$ - $TYPE$"
 	std::string regex_str;	// "Unmatched - (.*) - (.*)"
-	std::vector<std::string> replacements;	// { "$ID$", "$TYPE$" }
+	std::vector<Replacement> replacements;	// { ID, TYPE }
 
 	Rule ( std::string const& rule_str )
 	{
@@ -32,30 +110,94 @@ struct Rule
 		std::string str = rule_str; 
 		while (std::regex_search ( str, sm, rx)) {
 
-			std::cout << sm[0];
-			std::cout << std::endl;
+			//std::cout << sm[0];
+			//std::cout << std::endl;
 
-			this->replacements.push_back ( sm[0] ); // is it reliable to use 0th index???
+			this->replacements.push_back ( string2replacement ( sm[0] )); // is it reliable to use 0th index???
 
 			str = sm.suffix().str();
 		}
 
 		this->regex_str = std::regex_replace ( rule_str, rx, "(.*)" );
 
-		std::cout << std::endl << this->regex_str << std::endl;
+		//std::cout << std::endl << this->regex_str << std::endl;
 
+	}
+
+	ErrorData process_error ( std::string const& error_str ) const
+	{
+		ErrorData err_data;
+		err_data.valid = false;
+
+		std::regex const rx ( regex_str );
+		std::smatch sm;
+
+		if ( std::regex_match ( error_str, sm, rx ) && (sm.size() - 1) == replacements.size () )
+		{
+			for ( size_t i = 1; i < sm.size() ; ++i )
+			{
+				auto const& value = sm[i].str ();
+				auto replacement = replacements[i-1];
+
+				if ( !is_value_correct ( value, replacement ) ) // current rule doesn't match
+				{
+					err_data.valid = false;
+					break; 
+				}
+
+				err_data.valid = true;
+				switch ( replacement )
+				{
+				case ID:
+					err_data.id = value;
+					break;
+
+				case RESULT:
+					err_data.result = value;
+					break;
+
+				case TYPE:
+					err_data.type = value;
+					break;
+				}
+			}
+
+			if ( err_data.valid )
+			{
+				err_data.original = error_str;
+				err_data.rule_str = raw_str;
+			}
+		}
+
+		return err_data;
 	}
 };
 
-//std::vector<std::string> read_log ()
-//{
-//}
+// Should be read from TYPES.txt
+TYPES_CONT read_types ()
+{
+	std::string tmp[] = { "Solid Face", "Parabola", "Point", "Part Reference"};
+	TYPES_CONT types (tmp, tmp + sizeof(tmp) / sizeof(tmp[0]));
+
+	return types;
+}
+
+// Should be read from RESULTS.txt
+RESULTS_CONT read_results ()
+{
+	std::string tmp[] = { "Geometric Face", "Reference Name", "Trim Edge"};
+	RESULTS_CONT results (tmp, tmp + sizeof(tmp) / sizeof(tmp[0]));
+
+	return results;
+}
 
 std::vector<Rule> read_rules ()
 {
-	std::array<std::string, 2> rule_strings = {
+	std::array<std::string, 4> rule_strings = {
 		"Unmatched - $ID$ - $TYPE$",
-		"Unmatched - $TYPE$ - $ID$"
+		"Unmatched - $ID$ - $RESULT$",
+		"Unmatched - $TYPE$ - $RESULT$",
+		"Unmatched - $RESULT$ - $TYPE$",
 	};
 
 	std::vector<Rule> rules;
@@ -66,20 +208,58 @@ std::vector<Rule> read_rules ()
 	return rules;
 }
 
+void init ()
+{
+	rules = read_rules ();
+	types = read_types ();
+	results = read_results ();
+
+	// Print all rules
+	for ( auto const& rule : rules )
+		std::cout << rule.raw_str << std::endl;
+	std::cout << std::endl;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::vector<Rule> rules = read_rules ();
+	init ();
 
-	//std::regex const rx ( rule.regex_str );
-	//std::smatch sm;
-
-	std::array<std::string, 2> error_log = {
-		"Unmatched - Part Reference - Reference Name"
+	std::array<std::string, 5> error_strings = {
+		"Unmatched - 12 - Reference Name",
+		"Unmatched - 12 (10) - Reference Name",
+		"Unmatched - Part Reference - Reference Name",
+		"Unmatched - Reference Name - Part Reference",
+		"Unmatched - Reference Name - Part Reference - Reference Name",
 	};
-	
-	//if ( std::regex_match ( input_string, sm, rx ) )
-	//	for ( size_t i = 1; i < sm.size() ; ++i )
-	//			std::cout << sm[i] << std::endl;
+
+	for ( auto const& error_str : error_strings )
+	{
+		std::cout << "Error line: " << error_str << std::endl;
+		ErrorData err_data;
+
+		for ( auto const& rule : rules )
+		{
+			err_data = rule.process_error ( error_str );
+			if ( err_data.valid )
+				break;
+		} // iterate rules
+
+		if ( err_data.valid )
+		{
+			// Found correct Rule
+			// print results
+			std::cout << "MATCHED:" << std::endl;
+			std::cout << "\t Rule: " << err_data.rule_str << std::endl;
+			std::cout << std::endl;
+			std::cout << "\t ID: " << err_data.id << std::endl;
+			std::cout << "\t TYPE: " << err_data.type << std::endl;
+			std::cout << "\t RESULT: " << err_data.result << std::endl;
+		}
+		else
+			std::cout << "NO MATCHING RULE FOUND" << std::endl;
+		
+		std::cout << std::endl;
+	} // iterate errors
 
 	std::cout << std::endl;
 
